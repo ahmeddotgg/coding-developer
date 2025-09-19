cf9eb76d3e655cfd38088a1588baabbb61f9fde4
Ahmed Said <101777385+ahmeddotgg@users.noreply.github.com>
2025-09-19 12:32:41 +0300

    remove aoi routes

--- app/(landing)/_components/new-cources.tsx
+++ app/(landing)/_components/new-cources.tsx
@@ -1,10 +1,54 @@
 import { Clock } from "lucide-react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import type { Course } from "@/lib/db";
 
 export const NewCources = async () => {
-  const courses: Course[] = [];
+  const courses = [
+    {
+      id: 1,
+      title: "كورس أساسيات البرمجة باستخدام C++",
+      description:
+        "تعلم أساسيات البرمجة، البرمجة الكائنية، هياكل البيانات، ومهارات حل المشكلات باستخدام لغة C++.",
+      price: 3000,
+      discountPrice: 2000,
+      duration: "شهرين",
+      category: "Programming Fundamentals",
+      image: "/cources-images/c++.png",
+    },
+    {
+      id: 2,
+      title: "Frontend Development",
+      description:
+        "كورس متخصص في تطوير واجهات المستخدم باستخدام HTML, CSS, Bootstrap, Tailwind, JavaScript, وReact.",
+      price: 5000,
+      discountPrice: 4000,
+      duration: "3 شهور",
+      category: "Frontend",
+      image: "/cources-images/frontend.png",
+    },
+    {
+      id: 3,
+      title: "Backend Development",
+      description:
+        "كورس متخصص في تطوير السيرفر باستخدام PHP, MySQL, Laravel 12 مع تطبيقات عملية.",
+      price: 5000,
+      discountPrice: 4000,
+      duration: "3 شهور",
+      category: "Backend",
+      image: "/cources-images/backend.jpg",
+    },
+    {
+      id: 4,
+      title: "كورس الأطفال (4 مستويات)",
+      description:
+        "رحلة تعليمية للأطفال تبدأ بـ Scratch Jr ثم Scratch وتصميم الألعاب البسيطة، يليها أساسيات تصميم المواقع بمستوى مبسط وتنتهي بمشروع عملي وشهادة.",
+      price: 3000,
+      discountPrice: 1500,
+      duration: "شهرين + شهرين تدريب مجاني",
+      category: "Kids",
+      image: "/cources-images/scratch.png",
+    },
+  ];
 
   return (
     <div className="container space-y-16 py-22">
@@ -13,7 +57,7 @@ export const NewCources = async () => {
         <Image src="/vector.svg" alt="vector doodle" width={120} height={20} />
       </h2>
       <div className="grid auto-rows-fr grid-cols-1 gap-4 lg:grid-cols-3 min-[630px]:grid-cols-2">
-        {courses.map((cource: Course) => (
+        {courses.map((cource) => (
           <div
             key={cource.id}
             className="flex h-full flex-col gap-4 rounded-xl border"
