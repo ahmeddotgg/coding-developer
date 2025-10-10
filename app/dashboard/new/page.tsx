a5a9bf107c3f155a5b11c3549e6e92fbc47b53b9
Ahmed Said <ahmeddsaidd@proton.me>
2025-10-10 08:29:25 +0300

    Refactor: Rename (admin) to dashboard, add UI components, update configs

---
+++ app/dashboard/new/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "اضافة منتج",
+};
+
+export default function New() {
+  return <div></div>;
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "لوحة التحكم",
+};
+
+export default function Page() {
+  return <div></div>;
+}
