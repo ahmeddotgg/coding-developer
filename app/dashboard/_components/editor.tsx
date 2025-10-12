d3dcc8929e816e50d05954b543e18f89fc00cc04
Ahmed Said <ahmeddsaidd@proton.me>
2025-10-12 12:22:00 +0300

    feat: enhance TipTap editor with placeholder and state management

--- app/dashboard/_components/editor.tsx
+++ app/dashboard/_components/editor.tsx
@@ -1,14 +1,17 @@
 "use client";
 
 import Highlight from "@tiptap/extension-highlight";
+import Placeholder from "@tiptap/extension-placeholder";
 import TextAlign from "@tiptap/extension-text-align";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { Loader } from "lucide-react";
 import { InputStyles } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
+import { useEditorStore } from "@/store/editor-store";
 import ToolBar from "./toolbar";
 
-export const Editor = ({
+export const TipTapEditor = ({
   content,
   invalid,
   onChange,
@@ -17,15 +20,21 @@ export const Editor = ({
   invalid: boolean;
   onChange: (value: string) => void;
 }) => {
+  const { setEditor } = useEditorStore();
   const editor = useEditor({
     extensions: [
       StarterKit.configure({
         heading: {
-          levels: [2, 3],
+          levels: [1, 2, 3],
         },
       }),
       Highlight,
-      TextAlign,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Placeholder.configure({
+        placeholder: "اكتب وصف تفصيلي عن المنتج...",
+      }),
     ],
     editorProps: {
       attributes: {
@@ -39,16 +48,48 @@ export const Editor = ({
     },
     content,
     immediatelyRender: false,
-    onUpdate: ({ editor }) => {
-      console.log(editor.getHTML());
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy() {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
       onChange(editor.getHTML());
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
     },
   });
 
   return (
     <>
-      <ToolBar editor={editor} />
-      <EditorContent editor={editor} />
+      <ToolBar />
+      {editor ? (
+        <EditorContent editor={editor} />
+      ) : (
+        <div
+          className={cn(
+            InputStyles,
+            "flex h-60 max-w-none items-center justify-center rounded-lg",
+          )}
+        >
+          <Loader className="size-12 animate-spin" />
+        </div>
+      )}
     </>
   );
 };
